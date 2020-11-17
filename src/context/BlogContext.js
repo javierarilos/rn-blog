c7634939fd2cc10bb34bf1ed6af304f861a34036
javierarilos <javier.arilos@gmail.com>
2020-11-17 06:54:31 +0100

    context now is objects list

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,8 +2,13 @@ import React, { createContext } from 'react';
 
 const BlogContext = React.createContext();
 
+const blogPosts = [
+    {title: "Blog post #1"},
+    {title: "Blog post #2"},
+]
+
 export const BlogProvider = ({ children }) => {
-    return <BlogContext.Provider value={5}>
+    return <BlogContext.Provider value={blogPosts}>
         {children}
     </BlogContext.Provider>
 };
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,13 +1,18 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
 
-    const value = useContext(BlogContext);
+    const blogPosts = useContext(BlogContext);
     
     return (<View>
-        <Text>IndexScreen: {value}</Text>
+        <FlatList
+            data={blogPosts} 
+            keyExtractor={(blogPost) => blogPost.title}
+            renderItem={({ item }) => <Text>{item.title}</Text>}
+         />
+        <Text>{blogPosts.length} Blog Posts</Text>
     </View>);
 }
 
